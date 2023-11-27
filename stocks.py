fd66afa05ae09edfe9bd1d404c33b60540609363
alexj <alexj@LAPTOP-7048P6HN>
2023-11-27 19:01:47 +1100

    first commit. Added all core functionality

--- stocks.py
+++ stocks.py
@@ -42,6 +42,7 @@ def determine_price_at_signal(bool_series, float_series):
     
     return result_series
 
+
 def map_buy_sell_actions(buy_prices, sell_prices):
     actions = []
 
@@ -88,10 +89,52 @@ def get_num_buy_sell_pairs(lst):
     return len(lst)
 
 
+def number_profit_trades(lst):
+    num_profit = 0
+    for i in lst:
+        if i > 0:
+            num_profit += 1
+
+    return num_profit
+
+
+def number_loss_trades(lst):
+    num_loss = 0
+    for i in lst:
+        if i <= 0:
+            num_loss += 1
+
+    return num_loss
+
+
+def pct_win_loss_trades(lst):
+    num_profit = 0
+    num_loss = 0
+    
+    for i in lst:
+        if i > 0:
+            num_profit += 1
+        else:
+            num_loss += 1
+
+    pct_won = num_profit / len(lst)
+    pct_lost = num_loss / len(lst)
+
+    return pct_won, pct_lost
+
+
+def greatest_profit(lst):
+    return max(lst)
+
+
+def greatest_loss(lst):
+    return min(lst)
+
+
 # Fetch historical data
 # formatted_date = datetime.today().date().strftime('%Y-%m-%d')
 # data = yf.download('AAPL', start='2020-01-01', end=formatted_date)
-data = yf.download('AAPL', period="5y")
+data = yf.download('AAPL', period="5y", interval="1d")
 data = data.drop("Adj Close", axis=1).round(2)
 
 # Strategy: Calculate moving averages
@@ -112,11 +155,32 @@ data['SellPrice'] = determine_price_at_signal(data['SellSignal'], data["Close"])
 
 # Grouping Buy and Sell Signals:
 buy_sell_pairs = map_buy_sell_actions(data['BuyPrice'], data['SellPrice'])
+print(f"Buy/Sell pairs: {buy_sell_pairs}")
 
 # Determining number or buy, sell pairs
 num_buy_sell_pairs = get_num_buy_sell_pairs(buy_sell_pairs)
+print(f"Number Buy/Sell pairs: {num_buy_sell_pairs}")
 
 # Determining profit:
 profit_per_trade = profit_calculator_per_instance(buy_sell_pairs)
-total_profit = profit_calculator(profit_per_trade)
+print(f"Profit per trade: {profit_per_trade}")
+
+num_profit_trades = number_profit_trades(profit_per_trade)
+print(f"Number of profitable trades: {num_profit_trades}")
+
+num_loss_trades = number_loss_trades(profit_per_trade)
+print(f"Number of loss trades: {num_loss_trades}")
+
+pct_win, pct_loss = pct_win_loss_trades(profit_per_trade)
+print(f"% trades won: {round(pct_win*100)}%")
+print(f"% trades loss: {round(pct_loss*100)}%")
+
+gp = greatest_profit(profit_per_trade)
+print(f"Greatest profit: ${gp}")
+
+gl = greatest_loss(profit_per_trade)
+print(f"Greatest loss: ${gl}")
+
+# total_profit = profit_calculator(profit_per_trade)
+# print(f"Total profit: {total_profit}")
 
