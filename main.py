024c3f96b061f8ccdea7d5784b2078cbe8f0b53b
alexj <alexj@LAPTOP-7048P6HN>
2023-12-09 17:37:04 +1100

    updated all core strategies to default construction

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from core import get_yf_stock_data, round_result
 from results import StrategyResult
 from trades import BuySellTrades
 
-from MA_50_200_day import Strategy
+# from MA_50_200_day import Strategy
+# from MACD_crossover import Strategy
+from MACD_crossover_inverted import Strategy 
 
 
      
@@ -14,7 +16,7 @@ if __name__ == "__main__":
     
     algo_defs = AlgorithmDefinition(data)
     
-    strategy = Strategy("MA_50_200_day", algo_defs)
+    strategy = Strategy("MACD_crossover", algo_defs)
     
     trades = BuySellTrades(strategy)
     
