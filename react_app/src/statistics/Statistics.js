e4d273d57f50a1b34d8c661e496a292b0b997d68
alexj <alexj@LAPTOP-7048P6HN>
2024-07-30 21:32:54 +1000

    added initial code to statistics pae

--- react_app/src/statistics/Statistics.js
+++ react_app/src/statistics/Statistics.js
@@ -3,7 +3,17 @@ import React from 'react';
 const Statistics = () => {
   return (
     <div>
-      <h3>Statistics Tab Content Goes Here</h3>
+    
+    <label for="Country_selector">Select Country from dropdown: </label>
+    <select id="Country_selector" name="Select_countries">
+        <option value="Blank">-</option>
+        <option value="Aus">Australia</option>
+        <option value="US">US</option>
+    </select>
+    <div id="message"></div>
+
+    <label for="Stock_selector">Select Stock from all: </label>
+    
     </div>
   );
 };
